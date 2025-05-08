05f585703371cc9f21dedcaf5591e5a2dc0b4b1a
sahathiya <salamkaraya4@gmail.com>
2025-05-08 22:22:20 +0530

    set up the axios

--- Client/bookingapp/src/Axios/axiosinstance.js
+++ Client/bookingapp/src/Axios/axiosinstance.js
@@ -4,9 +4,6 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'https://booking-sggw.onrender.com', 
   withCredentials: true,
-  headers: {
-    'Content-Type': 'application/json'
-  }
   
 });
 
