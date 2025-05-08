567255d83cc502ad57ba91930b1251e161928d48
sahathiya <salamkaraya4@gmail.com>
2025-05-08 20:47:33 +0530

    set up the hosted link

--- Server/Controllers/User/userController.js
+++ Server/Controllers/User/userController.js
@@ -230,14 +230,14 @@ const loginWithOtp = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: true,
+      secure: false,
       sameSite: "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
